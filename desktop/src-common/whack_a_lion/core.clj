b3f9520b2e6ddc253e2160a27d0fc366737e776f
DjebbZ <khalid.jebbari@gmail.com>
2014-08-18 19:23:41 +0200

    Display and update score for each touched lion

--- desktop/src-common/whack_a_lion/core.clj
+++ desktop/src-common/whack_a_lion/core.clj
@@ -4,6 +4,8 @@
             [play-clj.ui :refer :all]
             [play-clj.math :refer :all]))
 
+(declare whack-a-lion main-screen score-screen)
+
 (defn spawn-lion [screen]
   (let [lion (texture "lion.png")
         lion-w (texture! lion :get-region-width)
@@ -45,7 +47,9 @@
     (let [coords (input->screen screen input-x input-y)
           touched-lion (touched-lion coords entities)]
       (if touched-lion
-        (remove #(= touched-lion %) entities)
+        (do
+          (run! score-screen :update-score)
+          (remove #(= touched-lion %) entities))
         entities)))
 
   :on-resize
@@ -53,8 +57,32 @@
     (width! screen (* 4 256))
     entities))
 
+(defscreen score-screen
+  :on-show
+  (fn [screen entities]
+    (update! screen :renderer (stage) :camera (orthographic) :score 0)
+    (assoc (label "0" (color :white))
+           :x 5 :y 5 :label? true))
+
+  :on-render
+  (fn [screen entities]
+    (let [e (for [entity entities]
+              (case (:label? entity)
+                true (doto entity (label! :set-text (str (:score screen))))
+                entity))]
+      (render! screen e)))
+
+  :update-score
+  (fn [screen entities]
+    (update! screen :score (inc (:score screen)))
+    entities)
+
+  :on-resize
+  (fn [screen entities]
+    (width! screen (* 1 256))))
+
 (defgame whack-a-lion
   :on-create
   (fn [this]
-    (set-screen! this main-screen)))
+    (set-screen! this main-screen score-screen)))
 
